b5ca855efbc1d14329e23f9f3e639a48ac372f1b
Matias <mflombardi2000@gmail.com>
2020-07-03 16:25:07 -0300

    Slope as instance variable

--- src/backend/model/Line.java
+++ src/backend/model/Line.java
@@ -4,10 +4,12 @@ import javafx.scene.canvas.GraphicsContext;
 
 public class Line extends Figure {
     private final Point startPoint, endPoint;
+    private final double slope;
 
     public Line(Point startPoint, Point endPoint) {
         this.startPoint = startPoint;
         this.endPoint = endPoint;
+        this.slope = (endPoint.getY() - startPoint.getY()) / (endPoint.getX() - startPoint.getX());
     }
 
     @Override
@@ -17,7 +19,6 @@ public class Line extends Figure {
 
     @Override
     public boolean contains(Point point) {
-        double slope = (endPoint.getY() - startPoint.getY()) / (endPoint.getX() - startPoint.getX());
         double intercept = startPoint.getY() - slope * startPoint.getX(); // ordenada al origen
         return Double.compare(point.getY(), intercept + slope * point.getX()) == 0;
     }
